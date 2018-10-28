20e646fcd05244706c164859344c4d89988456fa
Philip <philip.cai@mail.utoronto.ca>
2018-10-27 22:06:07 -0400

    Added score-related storage and methods into AccountManager and Account.

--- Phase1/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/Account.java
+++ Phase1/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/Account.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * Stores a username, password, and corresponding save files.
+ * Stores a username, password, personal high scores, and corresponding save files.
  */
 public class Account {
 
@@ -17,6 +17,11 @@ public class Account {
      * The user's password.
      */
     private String password;
+
+    /**
+     * A hash map storing a list of games and corresponding high scores.
+     */
+    private HashMap<String, Long> max_scores = new HashMap<>();
 
     /**
      * A hash map storing a list of games and corresponding lists of saves.
@@ -50,21 +55,51 @@ public class Account {
     }
 
     /**
+     * Stores the given filename under the specified game.
+     */
+    void addSave(String game, String file_name){
+        ArrayList<String> new_saves = new ArrayList<>();
+        if(saves.containsKey(game)){
+            new_saves = saves.get(game);
+            new_saves.add(file_name);
+            saves.put(game, new_saves);
+        }
+        else {
+            new_saves.add(file_name);
+        }
+        saves.put(game, new_saves);
+    }
+
+    /**
      * Returns a list of the account saves for a game.
      */
     ArrayList<String> getSaves(String game){
         return saves.get(game);
     }
 
+    /**
+     * Sets the maximum score for the specified game.
+     */
+    void setMaxScore(String game, Long score){
+        max_scores.put(game, score);
+    }
+
+    /**
+     * Gets the maximum score for the specified game.
+     */
+    Long getMaxScore(String game){
+        return max_scores.get(game);
+    }
+
     @Override
     public boolean equals(Object other){
         if(other instanceof Account){
             Account other_account = (Account)other;
-            return username.equals(other_account.getName());
+            return username.equalsIgnoreCase(other_account.getName());
         }
         if(other instanceof String){
             String other_name = (String)other;
-            return username.equals(other_name);
+            return username.equalsIgnoreCase(other_name);
         }
         return false;
     }
--- Phase1/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/AccountManager.java
+++ Phase1/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/AccountManager.java
@@ -3,7 +3,7 @@ package fall2018.csc2017.GameCentre;
 import java.util.ArrayList;
 
 /**
- * Stores and manages a list of user accounts, including account creation and deletion.
+ * Stores and manages a list of user accounts, including data retrieval, account creation, and deletion.
  */
 
 class AccountManager {
@@ -23,11 +23,11 @@ class AccountManager {
     }
 
     /**
-     * If name is not in use, adds a new Account with name and password and returns true. Else, returns false.
+     * If username is not in use, adds a new Account with username and password and returns true. Else, returns false.
      */
-    boolean addAccount(String name, String pass){
-        if (accounts.contains(name)) {
-            accounts.add(new Account(name, pass));
+    boolean addAccount(String username, String password){
+        if (accounts.contains(username)) {
+            accounts.add(new Account(username, password));
             return true;
         }
         return false;
@@ -36,9 +36,9 @@ class AccountManager {
     /**
      * Removes the account with the specified username and returns true.
      */
-    boolean removeAccount(String user) {
-        if (accounts.contains(user)) {
-            accounts.remove(user);
+    boolean removeAccount(String username) {
+        if (accounts.contains(username)) {
+            accounts.remove(username);
             return true;
         }
         return false;
@@ -47,9 +47,9 @@ class AccountManager {
     /**
      * If an account with the given username and password is found, set it as active and return true. Else, return false.
      */
-    boolean login(String user, String password){
+    boolean login(String username, String password){
         for(Account selected: accounts){
-            if(selected.equals(user)){
+            if(selected.equals(username)){
                 if(selected.getPassword().equals(password)){
                     current = selected;
                     return true;
@@ -60,9 +60,31 @@ class AccountManager {
     }
 
     /**
-     * Returns the save for the provided game.
+     * Adds the given filename under the specified game under the current account.
+     */
+    void getSaves(String game, String filename){
+        current.addSave(game, filename);
+    }
+
+    /**
+     * Returns a list of saves for the specified game under the current account.
      */
     ArrayList<String> getSaves(String game){
         return current.getSaves(game);
     }
+
+    /**
+     * Sets the current account's max score for the specified game.
+     */
+    public void setMaxScore(String game, Long max_score) {
+        current.setMaxScore(game, max_score);
+    }
+
+    /**
+     * Gets the current account's max score for the specified game.
+     */
+    public long getMaxScore(String game) {
+        return current.getMaxScore(game);
+    }
+
 }
