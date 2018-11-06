873cc53232b66b42f21736fd8a4a1a50ba26c823
bryparker <brydon.parker@mail.utoronto.ca>
2018-11-06 10:52:26 -0500

    bug fix in testing helpers - moving each tile swap

--- Phase1/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/TestingHelpers.java
+++ Phase1/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/TestingHelpers.java
@@ -56,6 +56,7 @@ public class TestingHelpers {
      * Shuffle a few tiles.
      */
     public static void swapFirstTwoTiles(BoardManager boardManager) {
+        boardManager.scoreBoard.move();
         boardManager.getBoard().swapTiles(0, 0, 0, 1);
     }
 
