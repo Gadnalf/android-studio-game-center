fd1483bf76c4f966ff9583e801d707ec59c10a9e
bryparker <brydon.parker@mail.utoronto.ca>
2018-11-23 09:53:26 -0500

    starting score baord testing

---
+++ Phase1/GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/ScoreBoardTest.java
@@ -0,0 +1,38 @@
+package fall2018.csc2017.GameCentre;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScoreBoardTest {
+
+    User user1 = new User("bill");
+    User user2 = new User("bob");
+
+
+    @Before
+    public void setUp() {
+        
+    }
+
+    @Test
+    public void testUpdateScoreBoard() {
+    }
+
+    @Test
+    public void testIsNewGame() {
+    }
+
+    @Test
+    public void testIsNewGameForUser() {
+    }
+
+    @Test
+    public void testUpdateUserScore() {
+    }
+
+    @Test
+    public void testUpdateGameScores() {
+    }
+}
--- Phase1/GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/ScoreBoardTesting.java
+++ Phase1/GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/ScoreBoardTesting.java
@@ -1,103 +1,103 @@
-//package fall2018.csc2017.GameCentre;
-//
-//import org.junit.Test;
-//
-//import java.util.List;
-//
-//import static org.junit.Assert.*;
-//
-//
-//public class ScoreBoardTesting extends TestingSlidingTiles {
-//
-//    SlidingTileSettings slidingTileSettings;
-//    User user;
-//    ScoreBoard scoreBoard;
-//
-////    @Test
-////    public void testIsSolved2() {
-//////        super.testIsSolved();
-////        setUpCorrect();
-////        System.out.println(slidingTilesBoardManager.puzzleSolved());
-////        assertEquals(true, slidingTilesBoardManager.puzzleSolved());
-////        swapFirstTwoTiles();
-////        assertEquals(false, slidingTilesBoardManager.puzzleSolved());
-////    }
-//
-//    public void setUpCorrect() {
+package fall2018.csc2017.GameCentre;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+
+public class ScoreBoardTesting extends TestingSlidingTiles {
+
+    SlidingTileSettings slidingTileSettings;
+    User user;
+    ScoreBoard scoreBoard;
+
+//    @Test
+//    public void testIsSolved2() {
+////        super.testIsSolved();
+//        setUpCorrect();
+//        System.out.println(slidingTilesBoardManager.puzzleSolved());
+//        assertEquals(true, slidingTilesBoardManager.puzzleSolved());
+//        swapFirstTwoTiles();
+//        assertEquals(false, slidingTilesBoardManager.puzzleSolved());
+//    }
+
+    public void setUpCorrect() {
+        List<Tile> tiles = makeTiles();
+        Board board = new Board(tiles);
+        user = new User("jim");
+        slidingTileSettings =  new SlidingTileSettings(3, 4);
+        slidingTilesBoardManager = new SlidingTilesBoardManager(board, user, slidingTileSettings);
+        scoreBoard = slidingTilesBoardManager.getScoreBoard();
+        System.out.println(slidingTilesBoardManager.puzzleSolved());
+    }
+
+    @Test
+    public void testAddNewUserAndGame() {
+        setUpCorrect();
+        System.out.println(scoreBoard.getScore());
+
+        assertEquals(1, scoreBoard.perGameScoreBoard.size());
+        assertEquals(1, scoreBoard.perUserScoreBoard.size());
+    }
+
+    @Test
+    public void testSizesCorrect() {
+        setUpCorrect();
+        System.out.println(scoreBoard.perGameScoreBoard.get(slidingTileSettings.getGameId()).getMaxScore());
+        assertEquals(10, (int) scoreBoard.perGameScoreBoard.get(slidingTileSettings.getGameId()).getMaxScore());
+        System.out.println(scoreBoard.perUserScoreBoard.get(user.getUserName()).getMaxScore());
+        assertEquals(10, (int) scoreBoard.perUserScoreBoard.get(user.getUserName()).getMaxScore());
+    }
+
+//    @Test
+//    public void testSavingAndLoading(){
+//        //---------add one user to scoreboard
+//        setUpCorrect();
+//        System.out.println(slidingTilesBoardManager.puzzleSolved());
+//        //puzzle is solved so will add to the scoreboard
+//        //---------add another user to the scoreboard
 //        List<Tile> tiles = makeTiles();
 //        Board board = new Board(tiles);
-//        user = new User("jim");
-//        slidingTileSettings =  new SlidingTileSettings(3, 4);
-//        slidingTilesBoardManager = new SlidingTilesBoardManager(board, user, slidingTileSettings);
-//        scoreBoard = slidingTilesBoardManager.getScoreBoard();
+//        scoreBoard = new ScoreBoard(
+//                new User("bill", "testing"),
+//                new SlidingTileSettings(4)
+//        );
+//        slidingTilesBoardManager = new SlidingTilesBoardManager(board, scoreBoard);
+//        //puzzle solved so we add to the scoreboard
 //        System.out.println(slidingTilesBoardManager.puzzleSolved());
+//        //save this scoreboard
+//        saveToFile(SAVE_FILENAME);
+//        //wipe the scoreboard
+//        ScoreBoard.perGameScoreBoard = new ArrayList<>();
+//        ScoreBoard.perUserScoreBoard = new ArrayList<>();
+//        loadFromFile(SAVE_FILENAME);
+//        //------------check that we load properly
+//        assertEquals(2, scoreBoard.perGameScoreBoard.size());
+//        assertEquals(2, scoreBoard.perUserScoreBoard.size());
 //    }
-//
-//    @Test
-//    public void testAddNewUserAndGame() {
-//        setUpCorrect();
-//        System.out.println(scoreBoard.getScore());
 
-//        assertEquals(1, scoreBoard.perGameScoreBoard.size());
-//        assertEquals(1, scoreBoard.perUserScoreBoard.size());
-//    }
-//
 //    @Test
-//    public void testSizesCorrect() {
-//        setUpCorrect();
-//        System.out.println(scoreBoard.perGameScoreBoard.get(slidingTileSettings.getGameId()).getMaxScore());
-//        assertEquals(10, (int) scoreBoard.perGameScoreBoard.get(slidingTileSettings.getGameId()).getMaxScore());
-//        System.out.println(scoreBoard.perUserScoreBoard.get(user.getUserName()).getMaxScore());
-//        assertEquals(10, (int) scoreBoard.perUserScoreBoard.get(user.getUserName()).getMaxScore());
-//    }
-//
-////    @Test
-////    public void testSavingAndLoading(){
-////        //---------add one user to scoreboard
-////        setUpCorrect();
-////        System.out.println(slidingTilesBoardManager.puzzleSolved());
-////        //puzzle is solved so will add to the scoreboard
-////        //---------add another user to the scoreboard
-////        List<Tile> tiles = makeTiles();
-////        Board board = new Board(tiles);
-////        scoreBoard = new ScoreBoard(
-////                new User("bill", "testing"),
-////                new SlidingTileSettings(4)
-////        );
-////        slidingTilesBoardManager = new SlidingTilesBoardManager(board, scoreBoard);
-////        //puzzle solved so we add to the scoreboard
-////        System.out.println(slidingTilesBoardManager.puzzleSolved());
-////        //save this scoreboard
-////        saveToFile(SAVE_FILENAME);
-////        //wipe the scoreboard
-////        ScoreBoard.perGameScoreBoard = new ArrayList<>();
-////        ScoreBoard.perUserScoreBoard = new ArrayList<>();
-////        loadFromFile(SAVE_FILENAME);
-////        //------------check that we load properly
-////        assertEquals(2, scoreBoard.perGameScoreBoard.size());
-////        assertEquals(2, scoreBoard.perUserScoreBoard.size());
-////    }
-//
-////    @Test
-////    public void testUpdating() {
-////        setUpCorrect();
-////
-////        assertEquals(10, scoreBoard.perGameScoreBoard.get(0).getMaxScore());
-////        assertEquals(10, scoreBoard.perUserScoreBoard.get(0).getMaxScore());
-////    }
-//
-//
-//    @Test
-//    public void testTimeResets() {
+//    public void testUpdating() {
 //        setUpCorrect();
 //
-//
+//        assertEquals(10, scoreBoard.perGameScoreBoard.get(0).getMaxScore());
+//        assertEquals(10, scoreBoard.perUserScoreBoard.get(0).getMaxScore());
 //    }
-//
-//    @Test
-//    public void testScoreUpdatesForUserAndGame() {
-//        setUpCorrect();
-//
-//    }
-//}
-//
+
+
+    @Test
+    public void testTimeResets() {
+        setUpCorrect();
+
+
+    }
+
+    @Test
+    public void testScoreUpdatesForUserAndGame() {
+        setUpCorrect();
+
+    }
+}
+
