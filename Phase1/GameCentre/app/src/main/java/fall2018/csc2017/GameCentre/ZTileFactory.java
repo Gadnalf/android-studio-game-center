eb4d643dd526a01bfa4cdfa824587cfd6681c7e4
Hokyun <hokyun.park@mail.utoronto.ca>
2018-11-26 17:34:01 -0500

    fixed ZTileFactory duplicate code

--- Phase1/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/ZTileFactory.java
+++ Phase1/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/ZTileFactory.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package fall2018.csc2017.GameCentre;
 
 import java.util.ArrayList;
@@ -38,45 +38,3 @@ public class ZTileFactory extends AbstractTilesFactory{
         return tiles;
     }
 }
-=======
-package fall2018.csc2017.GameCentre;
-
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-public class ZTileFactory extends AbstractTilesFactory implements Serializable {
-    public List<Tile> getTiles(int boardSize) {
-        List<Tile> tiles = new ArrayList<>();
-        int numTiles = boardSize * boardSize;
-
-        if (boardSize == 3) {
-            for (int tileNum = 0; tileNum != numTiles; tileNum++) {
-                if (tileNum == 1 || tileNum == 2) {
-                    tiles.add(new TileAlpha(0));
-                } else {
-                    tiles.add(new TileAlpha(-1));
-                }
-            }
-        } else if (boardSize == 4) {
-            for (int tileNum = 0; tileNum != numTiles; tileNum++) {
-                if (tileNum == 1 || tileNum == 2) {
-                    tiles.add(new TileAlpha(0));
-                } else {
-                    tiles.add(new TileAlpha(-1));
-                }
-            }
-        } else if (boardSize == 5) {
-            for (int tileNum = 0; tileNum != numTiles; tileNum++) {
-                if (tileNum == 1 || tileNum == 2) {
-                    tiles.add(new TileAlpha(0));
-                } else {
-                    tiles.add(new TileAlpha(-1));
-                }
-            }
-        }
-        return tiles;
-    }
-}
->>>>>>> master
