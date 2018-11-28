39fb61bd68050ab6a70da1862fb320742565f262
Philip <philip.cai@mail.utoronto.ca>
2018-11-28 16:39:22 -0500

    Updated StartingActivity functions to support GameHub

--- Phase1/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/SlidingTilesStartingActivity.java
+++ Phase1/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/SlidingTilesStartingActivity.java
@@ -3,46 +3,19 @@ package fall2018.csc2017.GameCentre;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 
 /**
  * The initial activity for the sliding puzzle tile game.
  */
 public class SlidingTilesStartingActivity extends AppCompatActivity {
 
-//    /**
-//     * where we store the scoreboards
-//     */
-//    public final String APP_DATA_DIR = this.getExternalCacheDir().toString();
-//    public final String APP_DATA_DIR = this.getFilesDir().toString();
-    /**
-     * An account manager save file.
-     */
-    public static final String ACCOUNT_SAVE_FILENAME = "save_file_accounts.ser";
-
-    /**
-     * The main save file name.
-     */
-    public static String saveFilename = "save_file_st.ser";
-
-    /**
-     * The temporary save file name.
-     */
-    public static String tempSaveFilename = "temp_save_file_st.ser";
-
     /**
      * The board manager.
      */
-    private SlidingTilesBoardManager slidingTilesBoardManager;
+    private GameHub gameHub;
 
     /**
      * User name.
@@ -53,57 +26,45 @@ public class SlidingTilesStartingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_slidingtiles);
-        addStartButtonListener();
-        addLoadButtonListener();
-        addSaveButtonListener();
+        addNewGameButtonListener();
+        addLoadButtonListener(this);
+        addSaveButtonListener(this);
         user = savedInstanceState.getString("user");
+        setupStartingActivity();
+        addGameScoreBoardButton();
+        addUserScoreBoardButton();
     }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        saveFilename = user.concat("_save_file_st.ser");
-        tempSaveFilename = user.concat("_temp_save_file_st.ser");
+    private void setupStartingActivity() {
+//        make a temp file just in case there is none yet
+        SaveAndLoad.saveAllTemp(
+                new GameHub(
+                        new SlidingTilesBoardManager(
+                                user,
+                                new SlidingTilesSettings(4,4)),
+                        new SeaInvadersBoardManager(user,
+                                new SeaInvadersSettings(.5, .5)),
+                        new ZTileBoardManager( user,
+                                new ZTileSettings(4 ,4)),
+
+                        user),
+                this);
+
+//        load the board manager if it exists if not load the temp file
+        gameHub = SaveAndLoad.loadGameHubPermanent(
+                user,
+                this);
+        //save the new board manager as temp if its been loaded
+        SaveAndLoad.saveGameHubTemp(
+                gameHub,
+                this); //this will save the loaded board manager to tmp to be used in the other activities
     }
-
-//    private void setupStartingActivity() {
-        //load in the accounts
-//        loadAccountsFromFile(ACCOUNT_SAVE_FILENAME);
-//        if (accountManager == null) {
-//            accountManager = new AccountManager();
-//        }
-//        saveAccountsToFile(ACCOUNT_SAVE_FILENAME);
-
-        //make a temp file just in case there is none yet
-//        String user = accountManager.getName();
-//        SaveAndLoad.saveAllTemp(
-//                new GameHub(
-//                        new SlidingTilesBoardManager(
-//                                user,
-//                                new SlidingTilesSettings(4,4)),
-//                        new SeaInvadersBoardManager(user,
-//                                new SeaInvadersSettings(.5, .5)),
-//                        new ZTileBoardManager( user,
-//                                new ZTileSettings(4 ,4)),
-//
-//                        user),
-//                this);
-
-        //load the board manager if it exists if not load the temp file
-//        slidingTilesBoardManager = SaveAndLoad.loadGameHubPermanent(
-//                user,
-//                this);
-//        //save the new board manager as temp if its been loaded
-//        SaveAndLoad.saveGameHubTemp(
-//                slidingTilesBoardManager,
-//                this); //this will save the loaded board manager to tmp to be used in the other activities
-//    }
 
     /**
      * Activate the new game button.
      */
-    private void addStartButtonListener() {
-        Button startButton = findViewById(R.id.start_game_button);
+    private void addNewGameButtonListener() {
+        Button startButton = findViewById(R.id.new_game_button);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -112,39 +73,23 @@ public class SlidingTilesStartingActivity extends AppCompatActivity {
         });
     }
 
-//    /**
-//     * Activate the load button.
-//     */
-//    private void addLoadButtonListener(final AppCompatActivity appCompatActivity) {
-//        Button loadButton = findViewById(R.id.load_game_button);
-//        loadButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                slidingTilesBoardManager = SaveAndLoad.loadGameHubPermanent(
-//                        slidingTilesBoardManager.getUser(),
-//                        appCompatActivity);
-////                loadFromFile(SAVE_FILENAME);
-//                SaveAndLoad.saveGameHubTemp(
-//                        slidingTilesBoardManager, appCompatActivity);
-////                saveToFile(TEMP_SAVE_FILENAME);
-//                makeToastLoadedText();
-//                switchToGame();
-//            }
-//        });
-//    }
-
     /**
      * Activate the load button.
      */
-    private void addLoadButtonListener() {
-        Button loadButton = findViewById(R.id.LoadButton);
+    private void addLoadButtonListener(final AppCompatActivity appCompatActivity) {
+        Button loadButton = findViewById(R.id.load_game_button);
         loadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadFromFile(saveFilename);
-                saveToFile(tempSaveFilename);
+                gameHub = SaveAndLoad.loadGameHubPermanent(
+                        gameHub.getUser(),
+                        appCompatActivity);
+//                loadFromFile(SAVE_FILENAME);
+                SaveAndLoad.saveGameHubTemp(
+                        gameHub, appCompatActivity);
+//                saveToFile(TEMP_SAVE_FILENAME);
                 makeToastLoadedText();
-                switchToSetting();
+                switchToGame();
             }
         });
     }
@@ -156,66 +101,51 @@ public class SlidingTilesStartingActivity extends AppCompatActivity {
         Toast.makeText(this, "Loaded SlidingTileSettings", Toast.LENGTH_SHORT).show();
     }
 
-//    /**
-//     * Activate the save button.
-//     */
-//    private void addSaveButtonListener(final AppCompatActivity appCompatActivity) {
-//        Button saveButton = findViewById(R.id.save_game_button);
-//        saveButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                SaveAndLoad.saveGameHubPermanent(
-//                        slidingTilesBoardManager,
-//                        appCompatActivity);
-////                saveToFile(SAVE_FILENAME);
-//                SaveAndLoad.saveGameHubTemp(
-//                        slidingTilesBoardManager, appCompatActivity);
-////                saveToFile(TEMP_SAVE_FILENAME);
-//                makeToastSavedText();
-//            }
-//        });
-//    }
-
     /**
      * Activate the save button.
      */
-    private void addSaveButtonListener() {
-        Button saveButton = findViewById(R.id.SaveButton);
+    private void addSaveButtonListener(final AppCompatActivity appCompatActivity) {
+        Button saveButton = findViewById(R.id.save_game_button);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                saveToFile(saveFilename);
-                saveToFile(tempSaveFilename);
+                SaveAndLoad.saveGameHubPermanent(
+                        gameHub,
+                        appCompatActivity);
+//                saveToFile(SAVE_FILENAME);
+                SaveAndLoad.saveGameHubTemp(
+                        gameHub, appCompatActivity);
+//                saveToFile(TEMP_SAVE_FILENAME);
                 makeToastSavedText();
             }
         });
     }
 
-//    /**
-//     * Activate the game-specific scoreboard button.
-//     */
-//    private void addGameScoreBoardButton() {
-//        Button saveButton = findViewById(R.id.GameScoreBoardButton);
-//        saveButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                switchToGameScoreBoard();
-//            }
-//        });
-//    }
-//
-//    /**
-//     * Activate the game-specific scoreboard button.
-//     */
-//    private void addUserScoreBoardButton() {
-//        Button saveButton = findViewById(R.id.UserScoreBoardButton);
-//        saveButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                switchToUserScoreBoard();
-//            }
-//        });
-//    }
+    /**
+     * Activate the game-specific scoreboard button.
+     */
+    private void addGameScoreBoardButton() {
+        Button saveButton = findViewById(R.id.game_scoreboard_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToGameScoreBoard();
+            }
+        });
+    }
+
+    /**
+     * Activate the game-specific scoreboard button.
+     */
+    private void addUserScoreBoardButton() {
+        Button saveButton = findViewById(R.id.user_scoreboard_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToUserScoreBoard();
+            }
+        });
+    }
 
     /**
      * Display that a game was saved successfully.
@@ -230,84 +160,46 @@ public class SlidingTilesStartingActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        setupStartingActivity();
     }
 
-//    /**
-//     * Switch to the SlidingTileSetting to change the setting.
-//     */
-//    private void switchToSetting() {
-//        Intent tmp = new Intent(this, SlidingTilesSettingsActivity.class);
-//        SaveAndLoad.saveGameHubTemp(
-//                slidingTilesBoardManager, this);
-//        startActivity(tmp);
-//    }
-
-//    /**
-//     * Read the temporary board from disk.
-//     */
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        saveFilename = GameHubActivity.accountManager.getName().concat("_save_file.ser");
-//        tempSaveFilename = GameHubActivity.accountManager.getName().concat("_temp_save_file.ser");
-//        loadFromFile(tempSaveFilename);
-//    }
-//
-//    /**
-//     * Switch to the SlidingTilesGameActivity view to play the game.
-//     */
-//    private void switchToGame() {
-//        Intent tmp = new Intent(this, SlidingTilesGameActivity.class);
-//        saveToFile(tempSaveFilename);
-//        startActivity(tmp);
-//    }
-//
     /**
-     * Switch to the SlidingTilesSettings to set up a new game.
+     * Switch to the SlidingTileSetting to change the setting.
      */
     private void switchToSetting() {
         Intent tmp = new Intent(this, SlidingTilesSettingsActivity.class);
-        saveToFile(tempSaveFilename);
+        SaveAndLoad.saveGameHubTemp(
+                gameHub, this);
         startActivity(tmp);
     }
 
-
     /**
-     * Load the board manager from fileName.
-     *
-     * @param fileName the name of the file
+     * Switch to the SlidingTileSetting to change the setting.
      */
-    private void loadFromFile(String fileName) {
-
-        try {
-            InputStream inputStream = this.openFileInput(fileName);
-            if (inputStream != null) {
-                ObjectInputStream input = new ObjectInputStream(inputStream);
-                slidingTilesBoardManager = (SlidingTilesBoardManager) input.readObject();
-                inputStream.close();
-            }
-        } catch (FileNotFoundException e) {
-            Log.e("login activity", "File not found: " + e.toString());
-        } catch (IOException e) {
-            Log.e("login activity", "Can not read file: " + e.toString());
-        } catch (ClassNotFoundException e) {
-            Log.e("login activity", "File contained unexpected data type: " + e.toString());
-        }
+    private void switchToGame() {
+        Intent tmp = new Intent(this, SlidingTilesGameActivity.class);
+        SaveAndLoad.saveGameHubTemp(
+                gameHub, this);
+        startActivity(tmp);
     }
 
     /**
-     * Save the board manager to fileName.
-     *
-     * @param fileName the name of the file
+     * Switch to the SlidingTileSetting to change the setting.
      */
-    public void saveToFile(String fileName) {
-        try {
-            ObjectOutputStream outputStream = new ObjectOutputStream(
-                    this.openFileOutput(fileName, MODE_PRIVATE));
-            outputStream.writeObject(slidingTilesBoardManager);
-            outputStream.close();
-        } catch (IOException e) {
-            Log.e("Exception", "File write failed: " + e.toString());
-        }
+    private void switchToGameScoreBoard() {
+        Intent tmp = new Intent(this, GameScoreBoardActivity.class);
+        SaveAndLoad.saveGameHubTemp(
+                gameHub, this);
+        startActivity(tmp);
+    }
+
+    /**
+     * Switch to the SlidingTileSetting to change the setting.
+     */
+    private void switchToUserScoreBoard() {
+        Intent tmp = new Intent(this, UserScoreBoardActivity.class);
+        SaveAndLoad.saveGameHubTemp(
+                gameHub, this);
+        startActivity(tmp);
     }
 }
